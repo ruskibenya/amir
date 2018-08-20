7df0c51119b2946fe4ca0bb70de66801bf0af166
rubencattan <rubencattan5@gmail.com>
2018-08-20 21:26:37 +0300

    fixed the schema

---
+++ db/migrate/20180820182430_remove_status_from_tours.rb
@@ -0,0 +1,5 @@
+class RemoveStatusFromTours < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :tours, :status, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_20_133506) do
+ActiveRecord::Schema.define(version: 2018_08_20_182430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,14 +48,11 @@ ActiveRecord::Schema.define(version: 2018_08_20_133506) do
   create_table "tours", force: :cascade do |t|
     t.datetime "start_time"
     t.datetime "end_time"
-    t.date "start_date"
-    t.date "end_date"
     t.string "name"
     t.bigint "user_id"
     t.bigint "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
     t.index ["group_id"], name: "index_tours_on_group_id"
     t.index ["user_id"], name: "index_tours_on_user_id"
   end
@@ -70,6 +67,7 @@ ActiveRecord::Schema.define(version: 2018_08_20_133506) do
     t.datetime "updated_at", null: false
     t.string "profile_picture"
     t.string "bio"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
