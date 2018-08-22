fdaab33895f2b6a97d1688ca446f000d437edc31
Ephraim Bismuth <ephraim_bismuth@hotmail.fr>
2018-08-22 16:04:50 +0300

    updating roots depending on signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  authenticated :user do
+    root 'tours#index', as: :authenticated_root
+  end
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tours do
