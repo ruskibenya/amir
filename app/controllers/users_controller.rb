aa5557d0ffd5006296568a8af4ab4f54ec46012e
Ephraim Bismuth <ephraim_bismuth@hotmail.fr>
2018-08-26 17:51:19 +0300

    javascript up and running on the visitor show page to take the visitor location and add it to the user latitude and longitude

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,11 @@ class UsersController < ApplicationController
   def show
     @user = current_user
   end
+
+  def update
+    @user = current_user
+    @user.latitude = params[:latitude]
+    @user.longitude = params[:longitude]
+    @user.save!
+  end
 end
