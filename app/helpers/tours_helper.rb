5558cd14660533d831f171a580936e516b0edf03
Benjamin Aronov <ruskibear31@gmail.com>
2018-08-26 10:06:50 +0300

    changed icon for government tours

--- app/helpers/tours_helper.rb
+++ app/helpers/tours_helper.rb
@@ -7,7 +7,7 @@ module ToursHelper
       "Business" => '<i class="fas fa-user-tie"></i>',
       "Pub Crawl" => '<i class="fas fa-beer"></i>',
       "Nature" => '<i class="fas fa-binoculars"></i>',
-      "Government" => '<i class="fas fa-poo"></i>',
+      "Government" => '<i class="fas fa-university"></i>',
       "Other" => '<i class="fas fa-map-marked-alt"></i>'
     }
 
